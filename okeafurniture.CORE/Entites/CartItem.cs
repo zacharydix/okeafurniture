953ab9e97bf1dbd51d15781af0e81565e5d5dc49
clara1010 <clara.schall@outlook.com>
2021-06-01 14:33:56 -0500

    added Cart and CartItem entities

---
+++ okeafurniture.CORE/Entites/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okeafurniture.CORE.Entites
+{
+    public class CartItem
+    {
+        public int CartId { get; set; }
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+
+
+        public Cart Cart { get; set; }
+        public Item Item { get; set; }
+
+    }
+}
