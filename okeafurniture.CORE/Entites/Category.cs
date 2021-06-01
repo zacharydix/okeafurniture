63b0ea31d46edcdd9f79bdf1382eff02450ef9ac
Kaitlyn Curtis <kaitlyn.curtis@gmail.com>
2021-06-01 14:29:20 -0500

    defined Category and ItemCategory entities

---
+++ okeafurniture.CORE/Entites/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okeafurniture.CORE.Entites
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public List<ItemCategory> ItemCategories { get; set; }
+
+        public Category()
+        {
+            ItemCategories = new List<ItemCategory>();
+        }
+    }
+}
