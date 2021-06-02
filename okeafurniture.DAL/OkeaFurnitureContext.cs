7ce851ed1dfb677ec9ef0e70bb19e40ac54e03a5
zacharydix <ZDix@dev-10.com>
2021-06-02 12:00:10 -0500

    added DAL and DAL testing layers, context, user secrets

---
+++ okeafurniture.DAL/OkeaFurnitureContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using okeafurniture.CORE.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okeafurniture.DAL
+{
+    public class OkeaFurnitureContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        
+        //bridge tables
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<ItemCategory> ItemCategories { get; set; }
+
+        public OkeaFurnitureContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CartItem>().HasKey(ci => new { ci.CartId, ci.ItemId });
+            modelBuilder.Entity<ItemCategory>().HasKey(ic => new { ic.ItemId, ic.CategoryId });
+        }
+    }
+}
---
+++ okeafurniture.WEB/okeafurniture.DAL.Tests/EFAccountRepositoryTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace okeafurniture.DAL.Tests
+{
+    public class EFAccountRepositoryTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
