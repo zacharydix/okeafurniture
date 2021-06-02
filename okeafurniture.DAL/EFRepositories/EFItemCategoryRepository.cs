322c95b408233d203340770255e0d2e6d37cb3a7
Kaitlyn Curtis <kaitlyn.curtis@gmail.com>
2021-06-02 13:55:18 -0500

    defined EFItemCategoryRepository, test project, and installed InMemoryDB nuget package

---
+++ okeafurniture.DAL/EFRepositories/EFItemCategoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using okeafurniture.CORE.Entites;
+using okeafurniture.CORE.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okeafurniture.DAL.EFRepositories
+{
+    public class EFItemCategoryRepository : IItemCategoryRepository
+    {
+        public Response<ItemCategory> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<List<ItemCategory>> GetAllByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<ItemCategory> Add(ItemCategory itemCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response Update(ItemCategory itemCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ okeafurniture.WEB/okeafurniture.DAL.Tests/EFItemCategoryRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using okeafurniture.CORE.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okeafurniture.DAL.Tests
+{
+    public class EFItemCategoryRepositoryTests
+    {
+        IItemCategoryRepository repository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<OkeaFurnitureContext>()
+                .UseInMemoryDatabase(databaseName: "FieldAgent").Options;
+            var context = new OkeaFurnitureContext(options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            repository = new EFAgentRepository(context);
+            mission = new Mission()
+        }
+    }
+}
