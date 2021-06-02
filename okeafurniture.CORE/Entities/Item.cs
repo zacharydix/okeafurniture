ef43c17e6c9b744d51ca18affaaf24de4f09dc61
Kaitlyn Curtis <kaitlyn.curtis@gmail.com>
2021-06-02 14:28:34 -0500

    removed CategoryId property from Item entity file

--- okeafurniture.CORE/Entities/Item.cs
+++ okeafurniture.CORE/Entities/Item.cs
@@ -9,10 +9,7 @@ namespace okeafurniture.CORE.Entites
     {
         public int ItemId { get; set; }
         public string ItemName { get; set; }
-
-        public int CategoryId { get; set; }
         public string ItemDescription { get; set; }
-
         public decimal UnitPrice {get;set;}
 
         public List<ItemCategory> ItemCategory { get;set; }
