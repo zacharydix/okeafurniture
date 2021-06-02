53f2be73ec2c049314013201bb4cb4174d6d62e6
Kaitlyn Curtis <kaitlyn.curtis@gmail.com>
2021-06-02 11:32:59 -0500

    defined ICategoryRepository, added using statement to reference entities namespace to both files

---
+++ okeafurniture.CORE/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using okeafurniture.CORE.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace okeafurniture.CORE.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        public Response<Category> Get(int id);
+        public Response<List<Category>> GetAll(int categoryId);
+        public Response<Category> Add(Category category);
+		public Response Update(Category category);
+		public Response Delete(int id);
+
+    }
+}
