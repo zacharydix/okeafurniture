8edab406b334a8a8931a898a8541fa68ba52ec49
Zachary Dix <80426294+zacharydix@users.noreply.github.com>
2021-06-02 13:14:06 -0500

    Update IItemRepository.cs

--- okeafurniture.CORE/Interfaces/IItemRepository.cs
+++ okeafurniture.CORE/Interfaces/IItemRepository.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace okeafurniture.CORE.Interfaces
 {
-    class IItemRepository
+    public class IItemRepository
     {
+        Response<Item> Insert(Item item);
+        Response Update(Item item);
+        Response Delete(int itemId);
+        Response<Item> Get(int itemId);
+        Response<List<Item>> GetByCategory(int categoryId);
     }
 }
