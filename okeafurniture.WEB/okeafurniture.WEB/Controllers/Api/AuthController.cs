35134eadf802b98ddd518a03e53d6a9ff9d9d5e5
Zachary Dix <80426294+zacharydix@users.noreply.github.com>
2021-06-03 14:25:06 -0500

    Update AuthController.cs

--- okeafurniture.WEB/okeafurniture.WEB/Controllers/Api/AuthController.cs
+++ okeafurniture.WEB/okeafurniture.WEB/Controllers/Api/AuthController.cs
@@ -32,15 +32,8 @@ namespace okeafurniture.WEB.Controllers.Api
             }
 
             //get account by Email
-<<<<<<< HEAD
             var account = _accountRepo.GetByEmail(login.Email).Data;
-=======
-            //var account = _accountRepo.GetByEmail(login.Email);
->>>>>>> origin/kaitlyncurtis
-
-            //;
-            var account = _accountRepo.GetAll().Data.SingleOrDefault(a => a.Email == login.Email);
-
+            
             //if no user found, say "That email is not registered"
             if (account == null)
             {
