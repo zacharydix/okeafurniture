b8d88e0560fd8bdbfe2eb05b42a91dd7eed79f88
Zachary Dix <zach47d@gmail.com>
2021-06-03 15:55:03 -0500

    added api controllers for accounts and payments

---
+++ okeafurniture.WEB/okeafurniture.WEB/Controllers/Api/AccountsController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using okeafurniture.CORE.Entites;
+using okeafurniture.CORE.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace okeafurniture.WEB.Controllers.Api
+{
+    [Route("api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private IAccountRepository repository;
+
+        public AccountsController(IAccountRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet, Route("get/all")]
+        public IActionResult GetAccounts()
+        {
+            Response<List<Account>> response = repository.GetAll();
+            if (response.Success)
+            {
+                return Ok(response.Data);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+        }
+
+        [HttpGet, Route("get/id")]
+        public IActionResult GetAccountById(int id)
+        {
+            Response<Account> response = repository.Get(id);
+            if (response.Success)
+            {
+                return Ok(response.Data);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+        }
+
+        [HttpGet, Route("get/email")]
+        public IActionResult GetAccountByEmail(string email)
+        {
+            Response<Account> response = repository.GetByEmail(email);
+            if (response.Success)
+            {
+                return Ok(response.Data);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+        }
+
+        [HttpPost, Route("add")]
+        public IActionResult AddAccount(Account account)
+        {
+            Response<Account> response = repository.Add(account);
+            if (response.Success)
+            {
+                return CreatedAtRoute(nameof(GetAccountById), new { id = account.AccountId }, account);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+        }
+
+        [HttpPut, Route("edit")]
+        public IActionResult EditAccount(Account account)
+        {
+            Response<Account> response = repository.Get(account.AccountId);
+            if (!response.Success)
+            {
+                return NotFound($"Account {account.AccountId} not found");
+            }
+            Response updateResponse = repository.Update(account);
+            if (updateResponse.Success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(updateResponse.Message);
+            }
+        }
+
+        [HttpDelete, Route("delete")]
+        public IActionResult DeleteAccount(int id)
+        {
+            Response<Account> response = repository.Get(id);
+            if (!response.Success)
+            {
+                return NotFound($"Account {id} not found");
+            }
+            Response deleteResponse = repository.Delete(id);
+            if (deleteResponse.Success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(deleteResponse.Message);
+            }
+        }
+    }
+}
---
+++ okeafurniture.WEB/okeafurniture.WEB/Controllers/Api/PaymentMethodsController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using okeafurniture.CORE.Entites;
+using okeafurniture.CORE.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace okeafurniture.WEB.Controllers.Api
+{
+    [Route("api/[controller]")]
+    public class PaymentMethodsController : ControllerBase
+    {
+        private IPaymentMethodRepository repository;
+
+        public PaymentMethodsController(IPaymentMethodRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet, Route("get/id")]
+        public IActionResult GetPaymentMethodById(int id)
+        {
+            Response<PaymentMethod> response = repository.Get(id);
+            if (response.Success)
+            {
+                return Ok(response.Data);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+        }
+
+        [HttpGet, Route("get/user")]
+        public IActionResult GetPaymentMethodByUser(int accountId)
+        {
+            Response<List<PaymentMethod>> response = repository.GetByUser(accountId);
+            if (response.Success)
+            {
+                return Ok(response.Data);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+        }
+
+        [HttpPost, Route("add")]
+        public IActionResult AddPaymentMethod(PaymentMethod paymentMethod)
+        {
+            Response<PaymentMethod> response = repository.Add(paymentMethod);
+            if (response.Success)
+            {
+                return CreatedAtRoute(nameof(GetPaymentMethodById), new { id = paymentMethod.PaymentMethodId }, paymentMethod);
+            }
+            else
+            {
+                return BadRequest(response.Message);
+            }
+        }
+
+        [HttpPut, Route("edit")]
+        public IActionResult EditPaymentMethod(PaymentMethod paymentMethod)
+        {
+            Response<PaymentMethod> response = repository.Get(paymentMethod.PaymentMethodId);
+            if (!response.Success)
+            {
+                return NotFound($"Payment method {paymentMethod.PaymentMethodId} not found");
+            }
+            Response updateResponse = repository.Update(paymentMethod);
+            if (updateResponse.Success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(updateResponse.Message);
+            }
+        }
+
+        [HttpDelete, Route("delete")]
+        public IActionResult DeletePaymentMethod(int id)
+        {
+            Response<PaymentMethod> response = repository.Get(id);
+            if (!response.Success)
+            {
+                return NotFound($"Payment method {id} not found");
+            }
+            Response deleteResponse = repository.Delete(id);
+            if (deleteResponse.Success)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(deleteResponse.Message);
+            }
+        }
+    }
+}
