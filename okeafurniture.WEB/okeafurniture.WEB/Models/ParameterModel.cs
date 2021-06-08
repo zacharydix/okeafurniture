6951ad0bd57a83833096eb68d1be7308630ed9df
clara1010 <clara.schall@outlook.com>
2021-06-08 00:08:33 -0500

    completed controller methods and view for revenue report

---
+++ okeafurniture.WEB/okeafurniture.WEB/Models/ParameterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace okeafurniture.WEB.Models
+{
+    public class ParameterModel
+    {
+        [Required (ErrorMessage = "Please select a start date!")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "Please select an end date!")]
+        public DateTime EndDate { get; set; }
+
+        public ParameterModel() { }
+    }
+}
