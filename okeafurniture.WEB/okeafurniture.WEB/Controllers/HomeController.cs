1b2ed3bc00dac64e2bd0ae80a3c72f3a2b2c16b2
clara1010 <clara.schall@outlook.com>
2021-06-08 13:50:14 -0500

    added About view

--- okeafurniture.WEB/okeafurniture.WEB/Controllers/HomeController.cs
+++ okeafurniture.WEB/okeafurniture.WEB/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace okeafurniture.WEB.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
